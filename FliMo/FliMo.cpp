db7ca4aa8a94641f41813d6285355177c6d5eac3
SaltedFries <tranblueneverdie@gmail.com>
2025-06-12 15:13:07 -0700

    add background and the cardback (totally legit)

--- FliMo/FliMo.cpp
+++ FliMo/FliMo.cpp
@@ -8,15 +8,26 @@ int main()
     sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode({ width, height }), "FliMo");
 	window->setFramerateLimit(60);
 
-	sf::Texture texture;
-    if (!texture.loadFromFile("assets/card_back.png"))
+	sf::Texture backgroundTexture;
+    if (!backgroundTexture.loadFromFile("assets/paper_background.png"))
+    {
+        std::cerr << "Failed to load texture!" << std::endl;
+        return -1;
+    }
+    sf::Sprite backgroundSprite(backgroundTexture);
+	//Load the background the size of the window
+	backgroundSprite.setScale({ static_cast<float>(width) / backgroundTexture.getSize().x,
+        static_cast<float>(height) / backgroundTexture.getSize().y });
+
+	sf::Texture cardBackTexture;
+    if (!cardBackTexture.loadFromFile("assets/card_back.png"))
     {
         std::cerr << "Failed to load texture!" << std::endl;
         return -1;
 	}
-	sf::Sprite sprite(texture);
-    sprite.setPosition({ (width - texture.getSize().x) / 2.f, (height - texture.getSize().y) / 2.f });
-    sprite.setScale({ 4.0f, 4.0f }); // Scale the sprite to make it larger
+	sf::Sprite sprite(cardBackTexture);
+    sprite.setPosition({ (width - cardBackTexture.getSize().x) / 2.f, (height - cardBackTexture.getSize().y) / 2.f });
+    sprite.setScale({ 4.0f, 4.0f }); //Scale the sprite to make it larger
 
     while (window->isOpen())
     {
@@ -36,9 +47,10 @@ int main()
         }
 
 		//Rendering code
-		window->clear(sf::Color(135, 206, 235)); // Sky blue color
+		window->clear(sf::Color(135, 206, 235)); //Sky blue color
 
 		//Drawing code would go here
+		window->draw(backgroundSprite);
 		window->draw(sprite);
 
 		window->display();
